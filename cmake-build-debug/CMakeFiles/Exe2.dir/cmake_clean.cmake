18498b940feb2e748a7900d0fa9e23fc4ff12f11
Lucifer <leilei.chui@gmail.com>
2019-06-08 15:19:46 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/Exe2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exe2.dir/main.cpp.o"
+  "Exe2.pdb"
+  "Exe2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exe2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Parking-Management-System.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parking-Management-System.dir/main.cpp.o"
+  "Parking-Management-System.pdb"
+  "Parking-Management-System"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parking-Management-System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
